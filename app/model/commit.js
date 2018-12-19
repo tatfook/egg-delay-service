9222e36e4f6a06491aca8046f9b3b81393ea7335
William <williamzjb@163.com>
2018-12-19 17:14:51 +0800

    Update commit model

--- app/model/commit.js
+++ app/model/commit.js
@@ -1,26 +1,24 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const dbConfig = require('../config').mongoDB;
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
 
-mongoose.connect(dbConfig.url, dbConfig.options);
+  const ActionSchema = new Schema({
+    action: String,
+    file_path: String,
+    previous_path: String,
+    content: String,
+    encoding: { type: String, default: 'text' },
+  });
 
-const Schema = mongoose.Schema;
+  const CommitSchema = new Schema({
+    branch: { type: String, default: 'master' },
+    id: String,
+    actions: [ ActionSchema ],
+    commit_message: String,
+    author_name: String,
+  }, { timestamps: true });
 
-const ActionSchema = new Schema({
-  action: String,
-  file_path: String,
-  previous_path: String,
-  content: String,
-  encoding: { type: String, default: 'text' },
-});
-
-const CommitSchema = new Schema({
-  branch: { type: String, default: 'master' },
-  id: String,
-  actions: [ ActionSchema ],
-  commit_message: String,
-  author_name: String,
-}, { timestamps: true });
-
-module.exports = mongoose.model('Commit', CommitSchema);
+  return mongoose.model('Commit', CommitSchema);
+};
